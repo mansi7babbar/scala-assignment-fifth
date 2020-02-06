8ac75bedda65ec2d5ac0fadb886ccbe2eba1907f
mansi7babbar <mansi7babbar@gmail.com>
2020-02-06 10:52:05 +0530

    added scala assignment

--- src/main/scala/com/knoldus/MapOperations.scala
+++ src/main/scala/com/knoldus/MapOperations.scala
@@ -6,6 +6,23 @@ class MapOperations {
     }
   }
 
+  def oddEven(map: Map[Int, List[String]]): Map[Int, List[String]] = {
+    val map = Map(
+      1 -> List("Sunil", "Laxmi"),
+      2 -> List("Bhavya", "Sangeeta"),
+      3 -> List("Arun", "Sushmita"),
+      4 -> List("Jamwant")
+    )
+
+    map.foldLeft(Map.empty[Int, List[String]]){ (resultMap, mapElement) =>
+      val resultList = mapElement._1 match {
+        case mapElem if mapElem % 2 ==0 => mapElement._2.map(str => str(0)+"even")
+        case _ => mapElement._2.map(str => str(0)+"odd")
+      }
+      resultMap + (mapElement._1 -> resultList)
+    }
+  }
+
   def findLastElement(list: List[Int]): Int = {
     list.foldLeft(0) { (_, element) => element }
   }
@@ -17,6 +34,14 @@ object MapOperations extends App {
   val map = Map("anurag" -> 24, "daniel" -> 23, "anushka" -> 30)
   val str = "anu"
   println(mapOps.sumConditional(map, str))
+
+  val map1 = Map(
+    1 -> List("Sunil", "Laxmi"),
+    2 -> List("Bhavya", "Sangeeta"),
+    3 -> List("Arun", "Sushmita"),
+    4 -> List("Jamwant")
+  )
+  mapOps.oddEven(map1).foreach(mapElem => println(mapElem))
 
   val list = List(1, 2, 3, 4, 5, 6, 7, 8)
   println(mapOps.findLastElement(list))
